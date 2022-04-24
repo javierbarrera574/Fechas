54d6ad6930816e52f7d0635ab91dda59f0910bed
javier barrera <fcbjavier12@hotmail.com>
2022-04-23 23:44:12 -0300

    Programa Fecha Siguiente

---
+++ Fechas/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Fechas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btSiguiente = new System.Windows.Forms.Button();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtAño = new System.Windows.Forms.TextBox();
+            this.lblDia = new System.Windows.Forms.Label();
+            this.lblMes = new System.Windows.Forms.Label();
+            this.lblAño = new System.Windows.Forms.Label();
+            this.btLimpiar = new System.Windows.Forms.Button();
+            this.btCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDia
+            // 
+            this.txtDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDia.Location = new System.Drawing.Point(102, 73);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(138, 29);
+            this.txtDia.TabIndex = 0;
+            this.txtDia.TextChanged += new System.EventHandler(this.txtDia_TextChanged);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(295, 338);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 20);
+            this.lblResultado.TabIndex = 1;
+            // 
+            // btSiguiente
+            // 
+            this.btSiguiente.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSiguiente.Location = new System.Drawing.Point(440, 66);
+            this.btSiguiente.Name = "btSiguiente";
+            this.btSiguiente.Size = new System.Drawing.Size(113, 36);
+            this.btSiguiente.TabIndex = 2;
+            this.btSiguiente.Text = "Siguiente";
+            this.btSiguiente.UseVisualStyleBackColor = true;
+            this.btSiguiente.Click += new System.EventHandler(this.btSiguiente_Click);
+            // 
+            // txtMes
+            // 
+            this.txtMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMes.Location = new System.Drawing.Point(102, 175);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(138, 29);
+            this.txtMes.TabIndex = 3;
+            this.txtMes.TextChanged += new System.EventHandler(this.txtMes_TextChanged);
+            // 
+            // txtAño
+            // 
+            this.txtAño.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAño.Location = new System.Drawing.Point(102, 281);
+            this.txtAño.Name = "txtAño";
+            this.txtAño.Size = new System.Drawing.Size(138, 29);
+            this.txtAño.TabIndex = 4;
+            this.txtAño.TextChanged += new System.EventHandler(this.txtAño_TextChanged);
+            // 
+            // lblDia
+            // 
+            this.lblDia.AutoSize = true;
+            this.lblDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDia.Location = new System.Drawing.Point(156, 37);
+            this.lblDia.Name = "lblDia";
+            this.lblDia.Size = new System.Drawing.Size(32, 16);
+            this.lblDia.TabIndex = 5;
+            this.lblDia.Text = "Dia";
+            // 
+            // lblMes
+            // 
+            this.lblMes.AutoSize = true;
+            this.lblMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMes.Location = new System.Drawing.Point(156, 142);
+            this.lblMes.Name = "lblMes";
+            this.lblMes.Size = new System.Drawing.Size(37, 16);
+            this.lblMes.TabIndex = 6;
+            this.lblMes.Text = "Mes";
+            // 
+            // lblAño
+            // 
+            this.lblAño.AutoSize = true;
+            this.lblAño.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAño.Location = new System.Drawing.Point(156, 252);
+            this.lblAño.Name = "lblAño";
+            this.lblAño.Size = new System.Drawing.Size(35, 16);
+            this.lblAño.TabIndex = 7;
+            this.lblAño.Text = "Año";
+            // 
+            // btLimpiar
+            // 
+            this.btLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLimpiar.Location = new System.Drawing.Point(440, 168);
+            this.btLimpiar.Name = "btLimpiar";
+            this.btLimpiar.Size = new System.Drawing.Size(113, 36);
+            this.btLimpiar.TabIndex = 8;
+            this.btLimpiar.Text = "Limpiar";
+            this.btLimpiar.UseVisualStyleBackColor = true;
+            this.btLimpiar.Click += new System.EventHandler(this.btLimpiar_Click);
+            // 
+            // btCerrar
+            // 
+            this.btCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCerrar.Location = new System.Drawing.Point(440, 274);
+            this.btCerrar.Name = "btCerrar";
+            this.btCerrar.Size = new System.Drawing.Size(113, 36);
+            this.btCerrar.TabIndex = 10;
+            this.btCerrar.Text = "Cerrar";
+            this.btCerrar.UseVisualStyleBackColor = true;
+            this.btCerrar.Click += new System.EventHandler(this.btCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 487);
+            this.Controls.Add(this.btCerrar);
+            this.Controls.Add(this.btLimpiar);
+            this.Controls.Add(this.lblAño);
+            this.Controls.Add(this.lblMes);
+            this.Controls.Add(this.lblDia);
+            this.Controls.Add(this.txtAño);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.btSiguiente);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtDia);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btSiguiente;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.TextBox txtAño;
+        private System.Windows.Forms.Label lblDia;
+        private System.Windows.Forms.Label lblMes;
+        private System.Windows.Forms.Label lblAño;
+        private System.Windows.Forms.Button btLimpiar;
+        private System.Windows.Forms.Button btCerrar;
+    }
+}
+
---
+++ Fechas/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fechas
+{
+    public partial class Form1 : Form
+    {
+
+        int dias;
+
+        int meses;
+
+        int año;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btSiguiente_Click(object sender, EventArgs e)
+        {
+
+            dias = int.Parse(txtDia.Text);
+
+            meses = int.Parse(txtMes.Text);
+
+            año = int.Parse(txtAño.Text);
+
+            if (dias>=1 && dias<=31)
+            {
+                lblResultado.Text = "";
+            }
+            else
+            {
+
+                MessageBox.Show("ingrese un dia valido dentro del rango establecido");
+                lblResultado.Text = null;
+            }
+
+
+            if (meses >0 && meses <= 12)
+            {
+
+                if (meses == 2 && dias<=28)
+
+                    lblResultado.Text = "febrero";
+                else
+                {
+                    if (año % 4 == 0 && año % 100 != 0 || año % 400 == 0)
+                    {
+                        lblResultado.Text = "febrero";
+                    }
+                    else
+                    {
+                        lblResultado.Text = null;
+                        txtDia.Text = null;
+                        txtMes.Text = null;
+                        txtAño.Text = null;
+                        MessageBox.Show("Es un año promedio, no es bisiesto");
+                    }
+
+
+                }
+
+                if (meses == 3)
+
+                    lblResultado.Text = "marzo";
+
+                if (meses == 4)
+
+                    if (dias<=30)
+                    {
+                        lblResultado.Text = "abril";
+                    }
+                    else
+                    {
+                        lblResultado.Text = null;
+                        txtDia.Text = null;
+                        txtMes.Text = null;
+                        txtAño.Text = null;
+                        MessageBox.Show("este mes llega hasta 30 dias, no 31");
+                    }
+
+                    
+
+                if (meses == 5)
+
+                    lblResultado.Text = "mayo";
+
+                if (meses == 6)
+
+                    if (dias<=30)
+                    {
+                        lblResultado.Text = "junio";
+                    }
+                    else
+                    {
+                        lblResultado.Text = null;
+                        txtDia.Text = null;
+                        txtMes.Text = null;
+                        txtAño.Text = null;
+                        MessageBox.Show("este mes llega hasta 30 dias, no 31");
+                    }
+                 
+
+                if (meses == 7)
+
+                    lblResultado.Text = "julio";
+
+                if (meses == 8)
+
+                    lblResultado.Text = "agosto";
+
+                if (meses == 9)
+
+                    if (dias<=30)
+                    {
+                        lblResultado.Text = "septiembre"; 
+                    }
+                    else
+                    {
+                        lblResultado.Text = null;
+                        txtDia.Text = null;
+                        txtMes.Text = null;
+                        txtAño.Text = null;
+                        MessageBox.Show("este mes llega hasta 30 dias, no 31");
+                    }
+
+                if (meses == 10)
+
+                    lblResultado.Text = "octubre";
+
+                if (meses == 11)
+
+                    if (dias<=30)
+                    {
+                        lblResultado.Text = "noviembre"; 
+                    }
+
+                    else
+                    {
+                        lblResultado.Text = null;
+                        txtDia.Text = null;
+                        txtMes.Text = null;
+                        txtAño.Text = null;
+                        MessageBox.Show("este mes llega hasta 30 dias, no 31");
+                    }
+
+                if (meses == 12)
+
+                    lblResultado.Text = "diciembre";
+
+                if (meses==1)
+                {
+                    lblResultado.Text = "enero";
+                }
+
+               
+
+            }
+            else
+            {
+
+                MessageBox.Show("ingrese un mes valido");
+                lblResultado.Text = null;
+
+            }
+
+            lblResultado.Text = Convert.ToString(dias+1 + " de " +  lblResultado.Text + " de " + año);
+        }
+
+         
+
+        private void txtMes_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAño_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btLimpiar_Click(object sender, EventArgs e)
+        {
+
+            txtDia.Text = null;
+
+            txtMes.Text = null;
+
+            txtAño.Text = null;
+
+            lblResultado.Text = null;
+
+
+
+        }
+
+        private void btActual_Click(object sender, EventArgs e)
+        {
+
+          
+
+        }
+
+        private void btCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
